39db92daa7d3074671b5d86e5d47bb494ac041e3
JuliaNakano <jumartinsnakano@gmail.com>
2021-11-02 17:43:53 -0300

    Solução do Exercício Maior 2 números

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercicio_Maior2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double primeiro, segundo; 
+
+            Console.WriteLine("Esse programa tem por finalidade alisar qual dos números digitados é maior");
+            Console.WriteLine("Para iniciá-lo digite:.....");
+            
+            Console.Write("\nDigite o primeiro número:... ");
+            primeiro = Convert.ToDouble(Console.ReadLine()); 
+            
+            Console.Write("Digite o segundo número:.... ");
+            segundo = Convert.ToDouble(Console.ReadLine());
+
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            if (primeiro > segundo )
+            {
+                Console.WriteLine($"O maior número é {primeiro}");
+                Console.ResetColor();
+            
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine($"O maior número é {segundo}");
+                Console.ResetColor();
+            }
+
+
+        }
+    }
+}
